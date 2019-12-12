cce918bedc0a49620cd8932128c44efdb028f7bb
Mojca Hercog <mojca.hercog@gmail.com>
2019-12-12 18:06:10 +0100

    Add first files for webpage

---
+++ main.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>SmartNinja CSS lesson</title>
+    </head>
+
+    <body>
+        <p style="color: red; font-size: 24px; font-family: Arial">Red colored text</p>
+    </body>
+</html>
